cf59af6144e0edb5bed7d71bff0c54b08ab5fc99
Jhurl123 <34147439+Jhurl123@users.noreply.github.com>
2018-03-26 20:44:39 -0400

    Add files via upload

---
+++ stringComparison.cpp
@@ -0,0 +1,89 @@
+//============================================================================
+// Name        : stringComparison.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+string removeSpaces(string input);
+string findMistake(string fixedFirst, string fixedSecond);
+int main() {
+
+
+	string firstString;
+	string secondString;
+	string fixedFirst;
+	string fixedSecond;
+
+	cout << "Welcome to the string comparison Justin, Enter -999 if done comparing" << endl;
+	//while(firstString != "-999" || secondString != "-999"){
+
+	cout << "Please Enter the first string to be compared" << endl;
+	while(getline(cin, firstString) && !firstString.empty()){
+		fixedFirst += firstString;
+	}
+	fixedFirst = removeSpaces(fixedFirst);
+	cout << endl;
+	cout << fixedFirst;
+	cout << endl;
+
+
+
+
+	cout << "Please enter the second string" << endl;
+	while(getline(cin, secondString) && !secondString.empty()){
+			fixedSecond += secondString;
+		}
+	fixedSecond = removeSpaces(fixedSecond);
+	cout << endl;
+	cout << secondString;
+
+
+	if(fixedFirst!= fixedSecond){
+
+
+		cout << "The strings do not Match!";
+		findMistake(fixedFirst, fixedSecond);
+	}
+	else{
+		cout << "The Strings Match!";
+	}
+
+
+
+}
+
+
+string removeSpaces(string input)
+{
+  input.erase(std::remove(input.begin(),input.end(),' '),input.end());
+  input.erase(std::remove(input.begin(),input.end(),'\n'),input.end());
+  return input;
+}
+string findMistake(string fixedFirst, string fixedSecond)
+{
+	for(int i = 0; i < fixedFirst.length(); i ++){
+		for(int j = 0; j < fixedSecond.length(); j++) {
+
+			if (fixedFirst[i] != fixedSecond[j]) {
+
+
+			cout << " The Mistake is "  << fixedFirst[i] << " " << fixedSecond[j] << " at the ";
+			// + std::string(i) +" position of the string";
+			break;
+			}
+
+
+
+		}
+	}
+
+}
